5d1e8bb7a196f784903260dc6d427891f03b229a
ElxCoNe <jchernandez10ocm@gmail.com>
2026-03-06 14:50:30 -0500

    fix port and env

--- models/db.model.js
+++ models/db.model.js
@@ -5,7 +5,7 @@ const pool = new Pool({
     user: process.env.DB_USER || "postgres",
     password: process.env.DB_PASSWORD || "poisonteamiariwi",
     host: process.env.DB_HOST || "localhost",
-    port: parseInt(process.env.DB_PORT || "5432", 10),
+    port: parseInt(process.env.DB_PORT || "5433", 10),
     database: process.env.DB_NAME || "postgres",
     max: 10,
     idleTimeoutMillis: 30000,
